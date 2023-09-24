e7059f1d90a057b8c045771fa6021b9a493f455e
Sborgobello <145886443+Sborgobello@users.noreply.github.com>
2023-09-24 00:55:32 -0300

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,14 @@
+document.getElementById("Instagram").onclick = function () {
+    
+    {
+        window.location  = "https://instagram.com/sofiaborgobello?igshid=NzZhOTFlYzFmZQ=="
+      }
+
+}
+document.getElementById("Facebook").onclick = function () {
+    
+    {
+        window.location  = "https://www.facebook.com/soo.borgobello"
+      }
+
+}
